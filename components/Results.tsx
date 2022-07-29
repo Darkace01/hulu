eeb6c19dfc298779be34159ae48a23ec24fc4b65
Kazeem Quadri <quadrikazeem01@gmail.com>
2022-07-29 20:51:22 +0100

    forwardref and other things

--- components/Results.tsx
+++ components/Results.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import { Movie } from '../types';
 import Thumbnail from './Thumbnail';
+import FlipMove from 'react-flip-move';
 interface Props {
   requests: Movie[];
 }
 function Results({ requests }: Props) {
-  console.log(requests);
   return (
-    <div className='px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center '>
+    <FlipMove className='px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center '>
       {requests?.map((result) => (
         <Thumbnail key={result.id} result={result} />
       ))}
-    </div>
+    </FlipMove>
   );
 }
 
