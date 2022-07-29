72cd4f77ff4727bd01dfb7f4c122180792207d90
Kazeem Quadri <quadrikazeem01@gmail.com>
2022-07-29 20:41:12 +0100

    flex and grid system

--- components/Results.tsx
+++ components/Results.tsx
@@ -7,7 +7,7 @@ interface Props {
 function Results({ requests }: Props) {
   console.log(requests);
   return (
-    <div>
+    <div className='px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center '>
       {requests?.map((result) => (
         <Thumbnail key={result.id} result={result} />
       ))}
